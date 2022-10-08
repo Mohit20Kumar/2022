ad15bb7ef59343081c153071036b6719f3868b86
Mohit20Kumar <mohitkumar20021220@gmail.com>
2022-10-08 22:43:25 +0530

    crew changed a bit

--- src/components/crew/crew.jsx
+++ src/components/crew/crew.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
+// import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 import "./crew.css"
 import Container from "react-bootstrap/Container";
@@ -9,6 +11,11 @@ import bg from "../../images/tracks_bg.png";
 
 
 const Crew = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
     useEffect(() => {
         Aos.init({ duration: 2000 });
       }, []);
@@ -22,6 +29,46 @@ const Crew = () => {
       </Container>
       <Container>
                 {/* PUT YOUR CONTENT HERE */}
+                <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
       </Container>
         </div>
   )
