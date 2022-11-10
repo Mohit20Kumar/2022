ca841e8c6e97f489f558b9730041c5c21759dc8f
Gaurav <gauravksh1609@gmail.com>
2022-11-10 11:52:49 +0530

    Updated Timer

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./timer.css"
+import "./timer.css";
 
 const Timer = () => {
-    const SECOND = 1000;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
-const DAY = 24 * HOUR;
+  const SECOND = 1000;
+  const MINUTE = 60 * SECOND;
+  const HOUR = 60 * MINUTE;
+  const DAY = 24 * HOUR;
 
-setInterval(() => {
-    let diff = Date.parse("Nov 11, 2022 12:00:00") - Date.now();
+  setInterval(() => {
+    let diff = Date.parse("Nov 11, 2022 11:00:00") - Date.now();
     const days = Math.floor(diff / DAY);
     diff -= days * DAY;
     const hours = Math.floor(diff / HOUR);
@@ -17,15 +17,11 @@ setInterval(() => {
     diff -= mins * MINUTE;
     const secs = Math.floor(diff / SECOND);
     document.querySelector("#timer").innerHTML = `
-        <div>${days}<span>Days</span></div>
         <div>${hours}<span>Hours</span></div>
         <div>${mins}<span>Minutes</span></div>
         <div>${secs}<span>Seconds</span></div>`;
-}, SECOND);
-  return (
-    <div id="timer">
-    </div>
-  );
+  }, SECOND);
+  return <div id="timer"></div>;
 };
 
 export default Timer;
